d2145b4735c5e90d40f6a57bac564377f401e257
geopablus <36490905+geopablus@users.noreply.github.com>
2023-12-29 09:43:10 +0100

    actualizar iconos

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-var map = L.map('map').setView([40.376413, -3.735885], 16);
+var map = L.map('map').setView([40.376413, -3.735885], 16); // Creamos el mapa
+var rataIcon = L.icon({
+    iconUrl: 'images/rata.png',
+    iconSize: [38, 38], // Tamaño del icono, ajusta según sea necesario
+    iconAnchor: [22, 94], // Punto del icono que corresponde a la ubicación del marcador
+    popupAnchor: [-3, -76] // Punto desde el cual se abrirá el popup
+});
 
+var cucasIcon = L.icon({
+    iconUrl: 'images/cuca.png',
+    iconSize: [38, 38], // Tamaño del icono
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76]
+});
+
+// Cargamos el mapa base
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 19,attribution: '© OpenStreetMap contributors'}).addTo(map);
 
 function onEachFeature(feature, layer) {
@@ -23,7 +37,15 @@ fetch(geojsonURL)
     .then(response => response.json())
     .then(data => {
         L.geoJSON(data, {
+            pointToLayer: function (feature, latlng) {
+                var icon = feature.properties.Vector === 'RATAS' ? rataIcon : cucasIcon; // Escoge el icono basado en la propiedad 'Vector'
+                return L.marker(latlng, { icon: icon });
+            },
             onEachFeature: onEachFeature
         }).addTo(map);
     })
     .catch(error => console.log('Error al cargar el GeoJSON:', error));
+
+
+
+    
