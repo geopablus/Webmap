75486fbbe27e1a4022954963f3f902ede8b39081
geopablus <36490905+geopablus@users.noreply.github.com>
2023-12-29 19:00:11 +0100

    Sólo fullscreen y escala

--- js/script.js
+++ js/script.js
@@ -32,17 +32,17 @@ var baseMaps = {"OpenStreetMap": osmLayer,"Satellite": satelliteLayer}; // Carga
 // control cambio mapas base
 osmLayer.addTo(map);
 L.control.layers(baseMaps).addTo(map);
-L.control.scale().addTo(map); // Cargamos la escala
+L.control.scale({ position: 'bottomright' }).addTo(map); // Cargamos la escala
 // Añadir geocodificaciones al mapa
-const provider = new GeoSearch.EsriProvider(); // proveedor de geocodificación ESRI
-const searchControl = new GeoSearch.GeoSearchControl({
-    provider: provider,
-    style: 'bar',
-    autoComplete: true, // Opcional: habilita la autocompletación
-    autoCompleteDelay: 250, // Opcional: tiempo de retardo para la autocompletación
-});
-searchControl.addTo(map); // Añadir la barra de búsqueda
-L.control.polylineMeasure(options).addTo(map); // Añadir medición al mapa
+// const provider = new GeoSearch.EsriProvider(); // proveedor de geocodificación ESRI
+// const searchControl = new GeoSearch.GeoSearchControl({
+//     provider: provider,
+//     style: 'bar',
+//     autoComplete: true, // Opcional: habilita la autocompletación
+//     autoCompleteDelay: 250, // Opcional: tiempo de retardo para la autocompletación
+// });
+// searchControl.addTo(map); // Añadir la barra de búsqueda
+// L.control.polylineMeasure(options).addTo(map); // Añadir medición al mapa
 
 function onEachFeature(feature, layer) {
     layer.on('click', function () {
